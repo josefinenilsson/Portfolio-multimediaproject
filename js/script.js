e6dea89ce5b24caef76e6cd675872d045baa71de
josefinenilsson <josefinenilsson@Togetic.local>
2013-10-06 16:20:10 +0200

    Added Geolocation function and local host function

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* Geolocation */
+
+
+navigator.geolocation.getCurrentPosition(gotIt,aintGotIt);
+			
+function gotIt(pos) {
+	var myLatlng = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+	var mapOptions = {
+		zoom: 18,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			
+var marker = new google.maps.Marker({
+	position: myLatlng,
+	map: map,
+	title: 'You are here!'
+});
+}
+
+//google.maps.event.addDomListener(window, 'load', initialize);			
+function aintGotIt(error){
+alert(error.message);
+}
+
+
+/* Local storage */
+
+window.onload = function(){
+	if(localStorage.length>0){
+		document.querySelector("input").placeholder=localStorage.name;					
+		document.body.innerHTML="<h1>Welcome " + localStorage.name + '!</h1><button onclick="localStorage.clear();location.reload()">Clear memory</button>';
+	}
+}
+function remember(){
+	val = document.getElementById("name").value;
+	localStorage.name=val;
+	window.location.reload();
+}
+
+/* Show and hide classes css */
+
+.hidden{
+	display:none;
+}
+
+
+
+/* Interactive javascript*/
+
