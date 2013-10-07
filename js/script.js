2d187c02b20ac4875f21c0deee555d7c0cb509f5
Josefine Nilsson <tommygustavsson@wlanguest138219.eduroam.hj.se>
2013-10-07 16:21:18 +0200

    Collage is now working

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@
 window.onload = function(){
 	if(localStorage.length>0){
 		document.querySelector("input").placeholder=localStorage.name;					
-		document.body.innerHTML="<h1>Welcome " + localStorage.name + '!</h1><button onclick="localStorage.clear();location.reload()">Clear memory</button>';
+		document.write.innerHTML="<h1>Welcome " + localStorage.name + '!</h1><button onclick="localStorage.clear();location.reload()">Clear memory</button>';
 	}
 }
 function remember(){
