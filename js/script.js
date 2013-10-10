f3736e1c7bcfa4aea7850fc9afac3ce2c063a5be
josefinenilsson <josefinenilsson@wlanguest86160.eduroam.hj.se>
2013-10-10 12:03:36 +0200

    Tog bort geolocation

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-/* Geolocation */
-
-
-//navigator.geolocation.getCurrentPosition(gotIt,aintGotIt);
-			
-//function gotIt(pos) {
-//var myLatlng = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
-	//var mapOptions = {
-		//zoom: 18,
-		//center: myLatlng,
-		//mapTypeId: google.maps.MapTypeId.ROADMAP
-//}
-//var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-			
-//var marker = new google.maps.Marker({
-	//position: myLatlng,
-	//map: map,
-	//title: 'You are here!'
-//});
-//}
-
-//google.maps.event.addDomListener(window, 'load', initialize);			
-//function aintGotIt(error){
-//alert(error.message);
-//}
 
 
 /* Local storage */
@@ -31,10 +6,10 @@ window.onload = function(){
 	if(localStorage.length>0){
 		document.getElementById("name").value=localStorage.namn;
 		document.getElementById("email").value=localStorage.email;
+
 		
 		
-			
-	}
+}
 
 
 	
@@ -47,13 +22,6 @@ function remember(){
 	window.location.reload();
 }
 
-
-$('#send').click(function(){
-	myform=document.getElementById("myform");
-   myform.reset();
-});
-
-
 /* Show and hide classes css */
 
 //.hidden{
@@ -63,9 +31,7 @@ $('#send').click(function(){
 
 
 /* Interactive javascript*/
-$('#video').click(function(){
-   document.getElementById('video').play();
-});
+
 
 /*  Flexslider */
 
@@ -78,9 +44,9 @@ $(window).load(function() {
 
 
 //
-/*
+
 $('body').flipLightBox({ lightbox_flip_speed: 500, lightbox_border_color: '#666666' })
-*/
+
 
 
 
